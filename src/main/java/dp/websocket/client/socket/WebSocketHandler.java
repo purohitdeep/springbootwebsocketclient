27fd61b2147ed161cb40f744f4e77f68bc877454
Deep Purohit <purohit.deep@gmail.com>
2019-03-05 12:24:34 +0530

    Startup fix and counter added for web socket messages limit

--- src/main/java/dp/websocket/client/socket/WebSocketHandler.java
+++ src/main/java/dp/websocket/client/socket/WebSocketHandler.java
@@ -2,25 +2,31 @@ package dp.websocket.client.socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@Component
 public class WebSocketHandler extends TextWebSocketHandler {
+
+    private static int counter = 1;
 
     protected Logger logger = LoggerFactory.getLogger(WebSocketHandler.class);
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         logger.info("Connection has been established with websocket server. {}", session);
-        TextMessage message = new TextMessage("Hi, from websocket!");
-        session.sendMessage(message);
+        session.sendMessage(new TextMessage("Great! I am now connected!"));
     }
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         logger.info("Message received : {} ", message.getPayload());
-        session.sendMessage(new TextMessage("You have sent " + message.getPayload()));
+        if (counter < 2) {
+            session.sendMessage(new TextMessage("Message number " + counter));
+            counter++;
+        }
     }
 
 }
